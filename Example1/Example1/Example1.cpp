5ab586aacad581e116eb20aa1621e18e8b8b79a3
WangXin93 <wangxin19930411@163.com>
2017-01-13 23:07:24 +0800

    Draw image in SFML
    Its a sample to draw mushroom bouncing between window boundary in 1st
    chapter

--- Example1/Example1/Example1.cpp
+++ Example1/Example1/Example1.cpp
@@ -2,15 +2,23 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 #include <SFML/Graphics.hpp>
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "First Window!");
-	sf::RectangleShape rectangle(sf::Vector2f(128.0f, 128.0f));
-	rectangle.setFillColor(sf::Color::Red);
-	rectangle.setPosition(320, 240);
-	rectangle.setOrigin(rectangle.getSize().x / 2, rectangle.getSize().y / 2);
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Mushroom Window");
+	sf::Texture mushroomTexture;
+	if (!mushroomTexture.loadFromFile("mushroom.png")){
+		cout << "Error! Fail to load texture." << endl;
+		return -1;
+	}
+	sf::Sprite mushroom(mushroomTexture);
+	sf::Vector2u size = mushroomTexture.getSize();
+	mushroom.setOrigin(size.x / 2, size.y / 2);
+	mushroom.setColor(sf::Color(255, 0, 0, 255));
+	sf::Vector2f increment(0.1f, 0.1f);
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -19,8 +27,19 @@ int _tmain(int argc, _TCHAR* argv[])
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		window.clear(sf::Color::Black);
-		window.draw(rectangle);
+		
+		if ((mushroom.getPosition().x + (size.x / 2) > window.getSize().x && increment.x > 0)
+			|| (mushroom.getPosition().x - (size.x / 2) < 0 && increment.x < 0)){
+			increment.x = -increment.x;
+		}
+		if ((mushroom.getPosition().y + (size.y / 2) > window.getSize().y && increment.y > 0)
+			|| (mushroom.getPosition().y - (size.y / 2) < 0 && increment.y < 0)){
+			increment.y = -increment.y;
+		}
+
+		mushroom.setPosition(mushroom.getPosition() + increment);
+		window.clear(sf::Color(16,16,16,255));
+		window.draw(mushroom);
 		window.display();
 	}
 	return 0;
